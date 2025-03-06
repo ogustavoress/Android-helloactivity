900db31f13d082df9f9b2df33bc4bf0c4f8d3adc
Ewerton Carreira <ewertoncarreira@gmail.com>
2025-03-06 14:52:40 -0300

    Limpando códigos do Jetpack Composer e ajustando a versão do SDK (isso só ocorreu porque o SDK estava desatualizado)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "carreiras.com.github.helloactivity"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "carreiras.com.github.helloactivity"
--- app/src/main/java/carreiras/com/github/helloactivity/MainActivity.kt
+++ app/src/main/java/carreiras/com/github/helloactivity/MainActivity.kt
@@ -15,32 +15,6 @@ import carreiras.com.github.helloactivity.ui.theme.AndroidhelloactivityTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            AndroidhelloactivityTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidhelloactivityTheme {
-        Greeting("Android")
     }
 }
